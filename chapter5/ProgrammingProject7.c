0a1c4a2013edd9fa92c22a87945f8d16da542652
April Warren <=>
2026-04-06 23:27:40 -0230

    Better checking algorithm for chapter 5 problem 7

--- chapter5/ProgrammingProject7.c
+++ chapter5/ProgrammingProject7.c
@@ -8,6 +8,33 @@ int main(){
   int inputOne, inputTwo, inputThree, inputFour, smallest, largest;
   printf("Enter four integers: ");
   scanf("%d %d %d %d", &inputOne, &inputTwo, &inputThree, &inputFour);
+  // I believe there is a better way to do this. This has 6 comparisons, my original had 24 I believe.
+  if(inputOne >= inputTwo){
+    largest = inputOne;
+    smallest = inputTwo;
+  }else{
+    largest = inputTwo;
+    smallest = inputOne;
+  }
+
+  if(inputThree >= inputFour){
+    if(inputThree >= largest){
+      largest = inputThree;
+    }
+    if(inputFour <= smallest){
+      smallest = inputFour;
+    }
+  }else{
+    if(inputFour >= largest){
+      largest = inputFour;
+    }
+    if(inputThree <= smallest){
+      smallest = inputThree;
+    }
+  }
+  
+
+  /*
   if(inputOne >= inputTwo && inputOne >= inputThree && inputOne >= inputFour){
     largest = inputOne;
   }else if(inputTwo >= inputOne && inputTwo >= inputThree && inputTwo >= inputFour){
@@ -26,6 +53,8 @@ int main(){
   }else{
     smallest = inputFour;
   }
+  */
+
   printf("Largest: %d\n", largest);
   printf("Smallest: %d\n", smallest);
 }
